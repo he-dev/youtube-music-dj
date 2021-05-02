fde5bc9689a996742142944747aceb3fc5905deb
ᎬᏁᎶᎥᏁᎬᎬᏒ <bennyboss+github@gmail.com>
2021-05-02 12:41:50 +0200

    Clean-up code

--- src/skip-dislike.js
+++ src/skip-dislike.js
@@ -2,32 +2,31 @@ setTimeout(skipDislike, 500);
 
 function skipDislike() {
 
-    let options = {}
+    try {
+        let options = {}
 
-    chrome.storage.sync.get("options", data => {
-        Object.assign(options, data.options)
+        chrome.storage.sync.get("options", data => {
+            Object.assign(options, data.options)
 
-        if (!options.canSkipDislike) {
-            console.log("skip-dislike is disabled")
-            return
-        }
-
-        try {
-
-            let likeStatus = document.getElementById("like-button-renderer").getAttribute("like-status")
-            console.log(`like-status: ${likeStatus}`)
-
-            if (likeStatus === "DISLIKE") {
-                options.count = options.count ? Number(options.count) + 1 : 1
-                chrome.storage.sync.set({ options })
-                nextSong()
+            if (options.canSkipDislike) {
+                if (isDislike()) {
+                    options.count = options.count ? Number(options.count) + 1 : 1
+                    chrome.storage.sync.set({ options })
+                    nextSong()
+                }
+            } else {
+                console.log("skip-dislike is disabled")
             }
+        })
+    } catch (err) {
+        console.error(err);
+    }
+}
 
-        } catch (err) {
-            console.error(err);
-            console.log("music not running will try in 2 seconds");
-        }
-    })
+function isDislike() {
+    let likeStatus = document.getElementById("like-button-renderer").getAttribute("like-status")
+    console.log(`like-status: ${likeStatus}`)
+    return likeStatus === "DISLIKE"
 }
 
 function nextSong() {
