df136afb0559a00af0828ee52b01304e26a3afbc
ᎬᏁᎶᎥᏁᎬᎬᏒ <bennyboss+github@gmail.com>
2021-05-02 13:23:49 +0200

    Fix skip counter not updated

--- src/popup.js
+++ src/popup.js
@@ -1,10 +1,23 @@
+const options = {}
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    translate("mainTitle")
-    translate("canSkipDislikeLabel")
-    translate("skipCount", [options.count || 0])
+    chrome.storage.sync.get("options", data => {
+        Object.assign(options, data.options)
+        mainForm.canSkipDislike.checked = Boolean(options.canSkipDislike)
 
+        translate("mainTitle")
+        translate("canSkipDislikeLabel")
+        translate("skipCount", [options.count || 0])
+    })
+
+    mainForm.canSkipDislike.addEventListener("change", event => {
+        options.canSkipDislike = event.target.checked
+        chrome.storage.sync.set({ options })
+    })
 });
+
+
 
 function translate(id, placeholders) {
     document.getElementById(id).innerText = chrome.i18n.getMessage(id, placeholders)
